f14d43429dde074888bbdc816c499fa9515e9c9e
SwSaxena1 <swati.saxena@sherwin.com>
2025-05-12 16:15:11 +0530

    Update project-creation.py

--- project-creation.py
+++ project-creation.py
@@ -2,7 +2,7 @@ import os
 import requests
 
 # Fetch environment variables with a default value if not set
-GITHUB_TOKEN = os.getenv('GH_TOKEN')
+GITHUB_TOKEN = os.getenv('GH_TOKEN','default_value_if_not_set')
 ORG = "sherwin-williams-co"
 PROJECT_TITLE = "test_swati_project"
 
